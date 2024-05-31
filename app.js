fd8711f2fc4e348bced4a9880469d509bd956fed
Rimsha708 <rimshaliaquat708@gmail.com>
2024-05-31 12:17:46 -0700

    fix: port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const { Mongoose } = require("mongoose");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 50002999;
 
 app.get("/", (req, res) => {
   res.send("hello");
